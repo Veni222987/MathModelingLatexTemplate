7a6887fcef4f4b928ef3caec643d8e07c3c9e50e
IMVeni <1948160779@qq.com>
2023-04-17 18:20:35 +0800

    优化模板

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 - 改变附录代码插入格式，改为lstinputlisting，直接输入文件名，保护你的键盘CV键
 - 改变文献引用格式，采用gbt7714-2015格式，同时修正了开源模板中的学位论文出现[出版社不详]的问题
 - 增加目录以便其他比赛使用，修正目录与ref的颜色冲突
+- 修改图片目录
 
  
 
@@ -22,3 +23,16 @@
 \end{longtable}
 ```
 
+
+
+- 关于图片
+
+  这里制定了figures以及code/img作为引用图片的目录，可以不用写路径直接写文件名，图片如果是matlab生成的统一采用eps，统一按照如下形式保存
+
+  ```matlab
+  legend('plot1的标签','plot2标签')
+  set(gca, 'LineWidth',1.5,'Box', 'off');
+  saveas(gcf,'./img/eps','epsc')
+  ```
+
+  
--- code/sample.m
+++ code/sample.m
@@ -1,2 +1,14 @@
-%代码不要写太长，看着matlab的右侧线，一般是80字符以内%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-A=[1,2,3;4,5,6]
+%代码不要写太长，看着matlab的右侧线，一般是80字符以内%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+f1=@(x)sin(x)+cos(x);
+f2=@(x)cos(x)-sin(x);
+xarr=-100:0.1:100;
+y1arr=f1(xarr);
+y2arr=f2(xarr);
+plot(xarr,y1arr,"Color",'red',Marker='x')
+hold on;
+plot(xarr,y2arr,"Color",'blue',Marker='o')
+legend('f1','f2')
+set(gca, 'LineWidth',1.5,'Box', 'off');
+saveas(gcf,'./img/eps','epsc')
+%print('-depsc', 'figure.eps');
+hold off
